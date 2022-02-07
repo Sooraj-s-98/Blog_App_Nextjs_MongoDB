7d9b248c31381c4d60487b194ba5f700a6636c9d
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-02-07 13:31:35 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
-const { SEARCH_URL } = process.env
 
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.contactform.dev' : '',
-  reactStrictMode: true,
+  reactStrictMode: false,
 }
