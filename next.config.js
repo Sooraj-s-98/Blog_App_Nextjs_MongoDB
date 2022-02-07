dc6241005acafeed1c3bdec1412d27771bdf043f
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-02-07 13:28:15 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -5,20 +5,4 @@ module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.contactform.dev' : '',
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `/:path*`,
-      },
-      {
-        source: '/search',
-        destination: `${SEARCH_URL}/search`,
-      },
-      {
-        source: '/search/:path*',
-        destination: `${SEARCH_URL}/search/:path*`,
-      },
-    ]
-  },
 }
