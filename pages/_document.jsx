ce9ef2b99faa57f6c747975585777eef30e015d9
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-09-16 16:10:14 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -34,6 +34,15 @@ class MyDocument extends Document {
   })();
 
                 `}} />
+
+         <script dangerouslySetInnerHTML={{__html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid: 3160303 ,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+       })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
         </Head>
         <body>
           <Main />
