76daa96ea0aabff380e7dc305f97301172f49a92
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-11-01 20:36:47 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -19,30 +19,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&amp;display=swap"
             rel="stylesheet"
           />
- <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/99e1b5075bd6b15018b31c6b/script.js"></script> 
-                      <script type="text/javascript" dangerouslySetInnerHTML={{__html: ` 
-                      var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://cloudwaysappsmozilor.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/cloudwaysappsmozilor.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-
-                `}} />
-
-         <script dangerouslySetInnerHTML={{__html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid: 3160303 ,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-       })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
+    <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/09d4d64b0f3084ba73105370/script.js"></script>
         </Head>
         <body>
           <Main />
