24977cf458b6ad13a9fc04378a8f041adf8443b5
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2023-05-09 10:43:49 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,6 +20,12 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
    <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/86a4260e57d89e13fedc639f/script.js"></script>
+                               <script type="text/javascript" dangerouslySetInnerHTML={{__html: ` 
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="G7pvw1wqC6JkphMPbj8doqNk64DyX3xG";;analytics.SNIPPET_VERSION="4.15.3";
+  analytics.load("G7pvw1wqC6JkphMPbj8doqNk64DyX3xG");
+  analytics.page();
+  }}();
+  `}} />
                          <script type="text/javascript" dangerouslySetInnerHTML={{__html: ` 
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
