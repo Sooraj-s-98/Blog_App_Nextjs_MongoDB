1fdf95a8b51be21c09ab70095c0f3748b26388f7
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2023-08-01 12:12:22 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
     return (
       <Html >
         <Head>
-		<script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/be2ad3c3e7cd69449824f0ae/script.js"></script>
+			<script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/43895fdbb5a4bf14ba075efb/script.js"></script>
         </Head>
         <body>
           <Main />
